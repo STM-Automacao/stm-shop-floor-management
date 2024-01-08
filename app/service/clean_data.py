e8ab1b7971570356fd469f4adedf7dde42e66b96
brunotomaz-dev <tomaz.bruno@gmail.com>
2024-01-08 16:38:42 -0300

    feat: ajustadas as posições das paradas

--- app/service/clean_data.py
+++ app/service/clean_data.py
@@ -22,11 +22,9 @@ class CleanData:
     """
 
     def __init__(self):
-        self.df_cadastro = pd.DataFrame()
-        self.df_info = pd.DataFrame()
-        self.df_occ = pd.DataFrame()
+        pass
 
-    def clean_maq_cadastro(self, data: pd.DataFrame) -> pd.DataFrame:
+    def clean_maq_cadastro(self, df_cadastro: pd.DataFrame) -> pd.DataFrame:
         """
         Limpa os dados de cadastro das máquinas
 
@@ -57,47 +55,42 @@ class CleanData:
 
 
         """
-        self.df_cadastro = data.copy()
-
         # Remover rows onde a linha é 0
-        self.df_cadastro = self.df_cadastro[self.df_cadastro["linha"] != 0]
+        df_cadastro = df_cadastro[df_cadastro["linha"] != 0]
 
         # Remover linhas duplicadas (erros de cadastro)
-        self.df_cadastro = self.df_cadastro.drop_duplicates(
+        df_cadastro = df_cadastro.drop_duplicates(
             subset=["data_registro", "linha"], keep="first"
         )
 
         # Criar nova coluna combinando data e hora
-        self.df_cadastro["data_hora_registro"] = (
-            self.df_cadastro["data_registro"].astype(str)
+        df_cadastro["data_hora_registro"] = (
+            df_cadastro["data_registro"].astype(str)
             + " "
-            + self.df_cadastro["hora_registro"]
-            .astype(str)
-            .str.split(".")
-            .str[0]
+            + df_cadastro["hora_registro"].astype(str).str.split(".").str[0]
         )
 
         # Converter coluna data_hora_registro para datetime
-        self.df_cadastro["data_hora_registro"] = pd.to_datetime(
-            self.df_cadastro["data_hora_registro"], format="%Y-%m-%d %H:%M:%S"
+        df_cadastro["data_hora_registro"] = pd.to_datetime(
+            df_cadastro["data_hora_registro"], format="%Y-%m-%d %H:%M:%S"
         )
 
         # Remover colunas desnecessárias
-        self.df_cadastro = self.df_cadastro.drop(
+        df_cadastro = df_cadastro.drop(
             columns=["recno", "data_registro", "hora_registro"]
         )
 
         # Ordenar dataframe para facilitar trabalho futuro
-        self.df_cadastro = self.df_cadastro.sort_values(
+        df_cadastro = df_cadastro.sort_values(
             by=["maquina_id", "data_hora_registro"], ascending=[True, False]
         )
 
         # reiniciar o index
-        self.df_cadastro = self.df_cadastro.reset_index(drop=True)
+        df_cadastro = df_cadastro.reset_index(drop=True)
 
-        return self.df_cadastro
+        return df_cadastro
 
-    def clean_maq_info(self, data: pd.DataFrame) -> pd.DataFrame:
+    def clean_maq_info(self, df_info: pd.DataFrame) -> pd.DataFrame:
         """
         Limpa os dados de cadastro das máquinas
 
@@ -133,88 +126,79 @@ class CleanData:
 
         """
 
-        self.df_info = data.copy()
-
         # Ordenar por maquina_id e data_registro, hora_registro
-        self.df_info.sort_values(
+        df_info.sort_values(
             by=["maquina_id", "data_registro", "hora_registro"], inplace=True
         )
 
         # Criar nova coluna combinando data e hora
-        self.df_info["data_hora_registro"] = (
-            self.df_info["data_registro"].astype(str)
+        df_info["data_hora_registro"] = (
+            df_info["data_registro"].astype(str)
             + " "
-            + self.df_info["hora_registro"].astype(str).str.split(".").str[0]
+            + df_info["hora_registro"].astype(str).str.split(".").str[0]
         )
 
         # Descartar colunas desnecessárias
-        self.df_info = self.df_info.drop(
+        df_info = df_info.drop(
             columns=["recno", "data_registro", "hora_registro"]
         )
 
         # Corrigindo erros da DB (horário dos turnos) - vai se tornar obsoleto à partir de Fev/24
-        self.df_info.sort_values(
+        df_info.sort_values(
             by=["maquina_id", "data_hora_registro"], inplace=True
         )
-        self.df_info = self.df_info[
+        df_info = df_info[
             ~(
-                (self.df_info["turno"] == "VES")
-                & (
-                    self.df_info["maquina_id"]
-                    != self.df_info["maquina_id"].shift()
-                )
+                (df_info["turno"] == "VES")
+                & (df_info["maquina_id"] != df_info["maquina_id"].shift())
             )
         ]  # descarta a primeira linha se o turno for VES
 
         # Criar nova coluna status_change para identificar mudança de status
-        self.df_info["status_change"] = self.df_info["status"].ne(
-            self.df_info["status"].shift()
+        df_info["status_change"] = df_info["status"].ne(
+            df_info["status"].shift()
         )
 
         # Criar coluna para identificar a mudança de máquina
-        self.df_info["maquina_change"] = self.df_info["maquina_id"].ne(
-            self.df_info["maquina_id"].shift()
+        df_info["maquina_change"] = df_info["maquina_id"].ne(
+            df_info["maquina_id"].shift()
         )
 
         # criar coluna para consolidar mudança de status e de máquina
-        self.df_info["change"] = (
-            self.df_info["status_change"] | self.df_info["maquina_change"]
+        df_info["change"] = (
+            df_info["status_change"] | df_info["maquina_change"]
         )
 
         # Criar coluna para identificar a mudança de turno
-        self.df_info["turno_change"] = self.df_info["turno"].ne(
-            self.df_info["turno"].shift()
-        )
+        df_info["turno_change"] = df_info["turno"].ne(df_info["turno"].shift())
 
         # Atualizar coluna change para incluir mudança de turno
-        self.df_info["change"] = (
-            self.df_info["change"] | self.df_info["turno_change"]
-        )
+        df_info["change"] = df_info["change"] | df_info["turno_change"]
 
         # Agrupar por maquina e identificar data e hora da última mudança de status
-        self.df_info["change_time"] = (
-            self.df_info.groupby("maquina_id")["data_hora_registro"]
+        df_info["change_time"] = (
+            df_info.groupby("maquina_id")["data_hora_registro"]
             .shift(0)
-            .where(self.df_info["change"])
+            .where(df_info["change"])
         )
 
         # Remover as linhas onde change_time é nulo
-        self.df_info.dropna(subset=["change_time"], inplace=True)
+        df_info.dropna(subset=["change_time"], inplace=True)
 
         # Criar nova coluna com a data_hora_final do status
-        self.df_info["data_hora_final"] = (
-            self.df_info.groupby("maquina_id")["data_hora_registro"]
+        df_info["data_hora_final"] = (
+            df_info.groupby("maquina_id")["data_hora_registro"]
             .shift(-1)
-            .where(~self.df_info["maquina_change"])
+            .where(~df_info["maquina_change"])
         )
 
         # Atualizar coluna data_hora_final onde maquina_change é True
-        self.df_info.loc[
-            self.df_info["maquina_change"], "data_hora_final"
-        ] = self.df_info["change_time"].shift(-1)
+        df_info.loc[df_info["maquina_change"], "data_hora_final"] = df_info[
+            "change_time"
+        ].shift(-1)
 
         # Remover colunas desnecessárias
-        self.df_info.drop(
+        df_info.drop(
             columns=[
                 "status_change",
                 "maquina_change",
@@ -226,39 +210,35 @@ class CleanData:
         )
 
         # Remover linhas onde data_hora_final é nulo
-        self.df_info.dropna(subset=["data_hora_final"], inplace=True)
+        df_info.dropna(subset=["data_hora_final"], inplace=True)
 
         # Cria nova coluna tempo_registro_min para calcular o tempo de registro em minutos
-        self.df_info["tempo_registro_min"] = (
-            pd.to_datetime(self.df_info["data_hora_final"])
-            - pd.to_datetime(self.df_info["data_hora_registro"])
+        df_info["tempo_registro_min"] = (
+            pd.to_datetime(df_info["data_hora_final"])
+            - pd.to_datetime(df_info["data_hora_registro"])
         ).dt.total_seconds() / 60
 
         # Arredondar tempo_registro_min e converter para inteiro
-        self.df_info["tempo_registro_min"] = (
-            self.df_info["tempo_registro_min"].round(0).astype(int)
+        df_info["tempo_registro_min"] = (
+            df_info["tempo_registro_min"].round(0).astype(int)
         )
 
         # Incluir um status in_test para os casos onde o status true é menor que 10 minutos
-        self.df_info = self.df_info.astype(
+        df_info = df_info.astype(
             {"status": str}
         )  # para evitar erros de comparação
-        self.df_info.loc[
-            (self.df_info["status"] == "true")
-            & (self.df_info["tempo_registro_min"] <= 10),
+        df_info.loc[
+            (df_info["status"] == "true")
+            & (df_info["tempo_registro_min"] <= 10),
             "status",
         ] = "in_test"
 
         # Ajustar nomenclatura dos status
-        self.df_info.loc[
-            self.df_info["status"] == "true", "status"
-        ] = "rodando"
-        self.df_info.loc[
-            self.df_info["status"] == "false", "status"
-        ] = "parada"
+        df_info.loc[df_info["status"] == "true", "status"] = "rodando"
+        df_info.loc[df_info["status"] == "false", "status"] = "parada"
 
         # Remover colunas desnecessárias
-        self.df_info.drop(
+        df_info.drop(
             columns=[
                 "ciclo_1_min",
                 "ciclo_15_min",
@@ -271,11 +251,11 @@ class CleanData:
         )
 
         # Ajustar o index
-        self.df_info.reset_index(drop=True, inplace=True)
+        df_info.reset_index(drop=True, inplace=True)
 
-        return self.df_info
+        return df_info
 
-    def clean_maq_occ(self, data: pd.DataFrame) -> pd.DataFrame:
+    def clean_maq_occ(self, df_occ: pd.DataFrame) -> pd.DataFrame:
         """
         Limpa os dados de cadastro das máquinas
 
@@ -311,8 +291,6 @@ class CleanData:
 
         """
 
-        self.df_occ = data.copy()
-
         # Motivos de Parada
         motivos = {
             1: "Ajustes",
@@ -334,31 +312,32 @@ class CleanData:
         }
 
         # Unir as colunas de data e hora
-        self.df_occ["data_hora_registro"] = (
-            self.df_occ["data_registro"].astype(str)
+        df_occ["data_hora_registro"] = (
+            df_occ["data_registro"].astype(str)
             + " "
-            + self.df_occ["hora_registro"].astype(str).str.split(".").str[0]
+            + df_occ["hora_registro"].astype(str).str.split(".").str[0]
         )
 
         # Modificar coluna motivo_id para int
-        self.df_occ = self.df_occ.astype({"motivo_id": int})
+        df_occ = df_occ.astype({"motivo_id": int})
 
         # Criar coluna motivo_nome com base no dicionário motivos
-        self.df_occ["motivo_nome"] = self.df_occ["motivo_id"].map(motivos)
+        df_occ["motivo_nome"] = df_occ["motivo_id"].map(motivos)
 
         # Ajustar problema se a string estiver vazia
-        self.df_occ["problema"] = self.df_occ["problema"].replace("", np.nan)
+        df_occ["problema"] = df_occ["problema"].replace("", np.nan)
+        df_occ["solucao"] = df_occ["solucao"].replace("", np.nan)
 
         # Se o problema for nulo, copiar o motivo_nome para o problema,
         # exceto para os motivos 1, 7, 8, 9, 14,
-        self.df_occ.loc[
-            self.df_occ["problema"].isnull()
-            & ~self.df_occ["motivo_id"].isin([1, 7, 8, 9, 14]),
+        df_occ.loc[
+            df_occ["problema"].isnull()
+            & ~df_occ["motivo_id"].isin([1, 7, 8, 9, 14]),
             "problema",
-        ] = self.df_occ["motivo_nome"]
+        ] = df_occ["motivo_nome"]
 
         # Definir ordem das colunas
-        self.df_occ = self.df_occ[
+        df_occ = df_occ[
             [
                 "maquina_id",
                 "motivo_id",
@@ -370,4 +349,4 @@ class CleanData:
             ]
         ]
 
-        return self.df_occ
+        return df_occ
--- app/service/join_data.py
+++ app/service/join_data.py
@@ -3,6 +3,7 @@
 """
 
 import pandas as pd
+import numpy as np
 
 
 # cSpell: words solucao, usuario
@@ -12,51 +13,53 @@ class JoinData:
     """
 
     def __init__(self):
-        self.df_info = pd.DataFrame()
-        self.df_cadastro = pd.DataFrame()
-        self.df_occ = pd.DataFrame()
-        self.df_join = pd.DataFrame()
+        pass
 
     def join_info_occ(
-        self, occ: pd.DataFrame, info: pd.DataFrame
+        self, df_occ: pd.DataFrame, df_info: pd.DataFrame
     ) -> pd.DataFrame:
         """
         Une os dados de info e ocorrência
-        """
 
-        self.df_info = info.copy()
-        self.df_occ = occ.copy()
+        Args:
+            df_occ (pd.DataFrame): Dataframe com os dados de ocorrência
+            df_info (pd.DataFrame): Dataframe com os dados de info
+
+        Returns:
+            pd.DataFrame: Dataframe com os dados de info e ocorrência unidos
+
+        Examples:
+            >>> join_data = JoinData()
+            >>> df_info_occ = join_data.join_info_occ(df_occ, df_info)
+        """
 
         # Criar uma função para ser usada em cada linha do dataframe
         def merge_rows(row):
             # Selecionar rows onde a data_hora_registro de occ está entre
             # data_hora_registro e data_hora_final de info
             mask = (
-                (
-                    self.df_occ["data_hora_registro"]
-                    >= row["data_hora_registro"]
-                )
-                & (self.df_occ["data_hora_registro"] <= row["data_hora_final"])
-                & (self.df_occ["maquina_id"] == row["maquina_id"])
+                (df_occ["data_hora_registro"] >= row["data_hora_registro"])
+                & (df_occ["data_hora_registro"] <= row["data_hora_final"])
+                & (df_occ["maquina_id"] == row["maquina_id"])
             )
 
             # Se houver rows selecionadas, retornar uma serie contendo os valores
-            if self.df_occ.loc[mask].shape[0] > 0:
+            if df_occ.loc[mask].shape[0] > 0:
                 return pd.Series(
                     [
-                        self.df_occ.loc[mask, "motivo_id"].values[0],
-                        self.df_occ.loc[mask, "motivo_nome"].values[0],
-                        self.df_occ.loc[mask, "problema"].values[0],
-                        self.df_occ.loc[mask, "solucao"].values[0],
-                        self.df_occ.loc[mask, "data_hora_registro"].values[0],
-                        self.df_occ.loc[mask, "usuario_id"].values[0],
+                        df_occ.loc[mask, "motivo_id"].values[0],
+                        df_occ.loc[mask, "motivo_nome"].values[0],
+                        df_occ.loc[mask, "problema"].values[0],
+                        df_occ.loc[mask, "solucao"].values[0],
+                        df_occ.loc[mask, "data_hora_registro"].values[0],
+                        df_occ.loc[mask, "usuario_id"].values[0],
                     ]
                 )
             else:
                 return pd.Series([None, None, None, None, None, None])
 
         # Aplicar a função merge_rows em cada linha do dataframe
-        self.df_info[
+        df_info[
             [
                 "motivo_id",
                 "motivo_nome",
@@ -65,17 +68,116 @@ class JoinData:
                 "data_hora_registro_operador",
                 "usuario_id",
             ]
-        ] = self.df_info.apply(merge_rows, axis=1)
+        ] = df_info.apply(merge_rows, axis=1)
 
         # Ajustar para sempre que estiver parada por motivo
         # 3, 4, 5, 12, 15, 16, o status será rodando
-        self.df_info.loc[
-            (self.df_info["status"] == "in_test")
-            & (self.df_info["motivo_id"].shift(1).isin([3, 4, 5, 12, 15, 16])),
+        df_info.loc[
+            (df_info["status"] == "in_test")
+            & (df_info["motivo_id"].shift(1).isin([3, 4, 5, 12, 15, 16])),
             "status",
         ] = "rodando"
 
         # Ajustar o index
-        self.df_info.reset_index(drop=True, inplace=True)
+        df_info.reset_index(drop=True, inplace=True)
 
-        return self.df_info
+        return df_info
+
+    def adjust_position(self, df: pd.DataFrame) -> pd.DataFrame:
+        """
+        Ajusta a posição das colunas
+
+        Args:
+            df (pd.DataFrame): Dataframe com os dados
+
+        Returns:
+            pd.DataFrame: Dataframe com os dados ajustados
+
+        Examples:
+            >>> join_data = JoinData()
+            >>> df = join_data.adjust_position(df)
+        """
+
+        # Criar uma coluna parada_inicio como bool para identificar o início de uma parada
+        df["parada_inicio"] = df["status"].eq("parada") & df[
+            "status"
+        ].shift().eq("rodando")
+
+        # Criar uma coluna parada_anterior como bool para identificar a parada anterior
+        df["parada_anterior"] = (
+            df.groupby("maquina_id")
+            .apply(
+                lambda group: group.apply(
+                    lambda row: row.name if row["parada_inicio"] else np.nan,
+                    axis=1,
+                )
+            )
+            .reset_index(level=0, drop=True)
+        )
+
+        # Preencher os valores nulos de parada_anterior com o último valor válido
+        df["parada_anterior"] = df.groupby("maquina_id")[
+            ["parada_anterior"]
+        ].ffill()
+
+        # Criar colunas para preparar o movimento das linhas para correção
+        df["motivo_id_moved"] = df.groupby("parada_anterior")[
+            ["motivo_id"]
+        ].transform("first")
+        df["motivo_nome_moved"] = df.groupby("parada_anterior")[
+            ["motivo_nome"]
+        ].transform("first")
+        df["problema_moved"] = df.groupby("parada_anterior")[
+            ["problema"]
+        ].transform("first")
+        df["solucao_moved"] = df.groupby("parada_anterior")[
+            ["solucao"]
+        ].transform("first")
+        df["data_hora_registro_operador_moved"] = df.groupby(
+            "parada_anterior"
+        )[["data_hora_registro_operador"]].transform("first")
+        df["usuario_id_moved"] = df.groupby("parada_anterior")[
+            ["usuario_id"]
+        ].transform("first")
+
+        # Preencher valores nulos com os valores movidos
+        df["motivo_id"] = df["motivo_id"].fillna(df["motivo_id_moved"])
+        df["motivo_nome"] = df["motivo_nome"].fillna(df["motivo_nome_moved"])
+        df["problema"] = df["problema"].fillna(df["problema_moved"])
+        df["solucao"] = df["solucao"].fillna(df["solucao_moved"])
+        df["data_hora_registro_operador"] = df[
+            "data_hora_registro_operador"
+        ].fillna(df["data_hora_registro_operador_moved"])
+        df["usuario_id"] = df["usuario_id"].fillna(df["usuario_id_moved"])
+
+        # Remover o motivo_id, motivo_nome e problema caso o status seja rodando
+        df.loc[
+            df["status"] == "rodando",
+            [
+                "motivo_id",
+                "motivo_nome",
+                "problema",
+                "data_hora_registro_operador",
+                "usuario_id",
+            ],
+        ] = np.nan
+
+        # Preencher os valores nulos com np.nan
+        df.fillna(np.nan, inplace=True)
+
+        # Remover as colunas desnecessárias
+        df.drop(
+            columns=[
+                "parada_inicio",
+                "parada_anterior",
+                "motivo_id_moved",
+                "motivo_nome_moved",
+                "problema_moved",
+                "solucao_moved",
+                "data_hora_registro_operador_moved",
+                "usuario_id_moved",
+            ],
+            inplace=True,
+        )
+
+        return df
